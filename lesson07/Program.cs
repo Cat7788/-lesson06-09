2af46dfecd81821afc8d7c33c83b5055a87104d1
KAT <sergeeva0090@mail.ru>
2022-09-24 11:26:03 +0400

    Change

---
+++ lesson07/Program.cs
@@ -0,0 +1,132 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+/*double[,] CreateRandom2dArray()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    double[,] newArray = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(-9, 10) + Math.Round(new Random().NextDouble(), 2);  
+    return newArray;
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] < 0) Console.Write($"{array[i, j]} "); 
+            else Console.Write($"{array[i, j]}  "); 
+            Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+/*int[,] CreateRandom2dArray(int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(10, 100);
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void element(int i, int j, int[,] array, int rows, int columns)
+{
+    if (i < rows && j < columns)
+        Console.WriteLine($"Элемент массива на позиции ({i}, {j}) равен {array[i, j]}");
+    else
+        Console.WriteLine("Такого числа в массиве нет");
+}
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = CreateRandom2dArray(rows, columns);
+Show2dArray(myArray);
+Console.Write("Введите номер строки, в которой находится элемент: ");
+int ipos = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца, в котором находится элемент: ");
+int jpos = Convert.ToInt32(Console.ReadLine());
+element(ipos, jpos, myArray, rows, columns);*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+/*int[,] CreateRandom2dArray()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(10, 100);
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} "); 
+            Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double  summ = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+            summ += array[i, j];
+        double result = summ / array.GetLength(1);
+        Console.WriteLine($"Среднее арифметическое столба номер {j+1} равно {Math.Round(result, 2)}");
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray); 
+ArithmeticMean(myArray);*/
+
